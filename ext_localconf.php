55cb77fae576941f1e73375b60f1f2fab0a91620
Sebastian Schmal <sebastian.schmal@gmx.de>
2020-12-14 14:49:22 +0100

    Edit Ext config

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,8 +1,5 @@
 <?php
-
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+defined('TYPO3_MODE') or die();
 
 call_user_func(static function () {
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
